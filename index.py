dd1d5087892bb18a17695fa9563f18b50d4d4d9d
vinayakpt321 <vinayak.t@s.amity.edu>
2025-07-28 11:55:49 +0530

    made changes

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-text = input("Enter a string: ")
+text = input("Enter a string to check if it is palindrome: ")
 if text == text[::-1]:
     print("Palindrome")
     else:
         print("Not a Palindrome")
+
+
